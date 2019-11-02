8e5e652734f519628c22df2a094981377b2d18c7
fyxtxd <55904847+fyxtxd@users.noreply.github.com>
2019-11-02 11:28:05 +0800

    Add files via upload
    hello

---
+++ src/NoteApp.js
@@ -0,0 +1,30 @@
+//hello
+//hello
+
+import React from 'react';
+
+
+let notes = {
+    title: "Hello World (TITLE)",
+    body: "Hello World (BODY)"
+}
+
+const NoteApp = () => {
+    let [title, setTitle] = useState('')
+    let [body, setBody] = useState('')
+}
+let titleChange = (e) => {
+    console.log(e.target.value)
+}
+
+let bodyChange = (e) => {
+    setBody(e.target.value)
+}
+
+return (
+    <div>
+        Notes Title: <input type="text" onChange={titleChange}><</input>
+    </div>
+)
+
+np
---
+++ src/Notes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Notes = () => {
+    return (
+        <div>
+            <h1> {title}</h1>
+            <p>{body}</p>
+        </div>
+    )
+
+}
+
+export default Notes
